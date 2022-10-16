4a5c6c93b7e040b5344e4564b65acc4f393769b2
ksenia <knyazeva_2011@bk.ru>
2022-10-16 18:18:15 +0300

    Added program

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо 
+// равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении
+// не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+string[] ArrayLessThan3Characters(string[] array)
+{
+    string[] newArray = new string[array.Length];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= 3)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+    return newArray;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+string[] array = new string[7] { "55", "kkkk", "ooo", "546", "r", "88547", "11" };
+string[] newArray = ArrayLessThan3Characters(array);
+PrintArray(newArray);
